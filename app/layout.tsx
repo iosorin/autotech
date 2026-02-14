f41773e1e658eb50fd5a4c463d6cf7443da1ffe4
Мирослав Осорин <miroslav.osorin@gmail.com>
2026-02-14 20:19:36 +0500

    chore: savepoint

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,15 +25,18 @@ export const metadata: Metadata = {
     description: seo.defaultDescription,
     images: [{ url: seo.ogImage, width: 1200, height: 630, alt: seo.ogImageAlt }],
   },
+  manifest: "/site.webmanifest",
   icons: {
     icon: "/favicon/favicon.ico",
     apple: "/favicon/apple-touch-icon.png",
   },
-  manifest: "/favicon/site.webmanifest",
   twitter: {
     card: "summary_large_image",
     title: seo.defaultTitle,
     description: seo.defaultDescription,
+  },
+  verification: {
+    google: "BOArqqCtqsRqa5JhqbSN7ZSxkLVlpPNtPoGupnwPHiY",
   },
 };
 
