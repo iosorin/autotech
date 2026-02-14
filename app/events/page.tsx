f56874ab8b111bb06fe0db101dbc9fe79d5d38a9
Мирослав Осорин <miroslav.osorin@gmail.com>
2026-02-14 13:44:54 +0500

    chore: savepoint

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -12,21 +12,26 @@ export const metadata: Metadata = {
 export default function EventsPage() {
   const p = events;
   return (
-    <section id="events">
-      <div className="gradlayer bg-gradient-blue" />
+    <>
+      <section id="event">
+        <div className="gradlayer bg-gradient-blue" />
+        <Lead label={p.subtitle} title={p.title} titleFirst />
+        <Event
+          date={home.eventBanner.date}
+          title={home.eventBanner.title}
+          line1={home.eventBanner.line1}
+          line2={home.eventBanner.line2}
+          cta={home.eventBanner.cta}
+          button={home.eventBanner.button}
+          className="bg-white shadow-lg"
+        />
 
-      <Lead label={p.subtitle} title={p.title} titleFirst />
-      <Event
-        date={home.eventBanner.date}
-        title={home.eventBanner.title}
-        line1={home.eventBanner.line1}
-        line2={home.eventBanner.line2}
-        cta={home.eventBanner.cta}
-        button={home.eventBanner.button}
-        className="bg-white shadow-lg"
-      />
-      <Lead title={p.pastHeading} />
-      <List events={p.past} className="my-12" />
-    </section>
+      </section>
+      <section id="past">
+        <Lead title={p.pastHeading} />
+        <List events={p.past} className="my-12" />
+      </section>
+    </>
+
   );
 }
