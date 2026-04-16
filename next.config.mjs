75bb5c5590e587756fff4fd417ceb19e0db2a973
iosorin <miroslav.osorin@gmail.com>
2026-04-16 11:44:33 +0500

    chore: savepoint

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,20 @@ const nextConfig = {
         headers: [ 
           { key: 'X-Frame-Options', value: 'SAMEORIGIN' },
           { key: 'X-Content-Type-Options', value: 'nosniff' },
-          { key: 'Content-Security-Policy',value: 'default-src \'self\'; script-src \'self\' \'unsafe-inline\' \'unsafe-eval\'; style-src \'self\' \'unsafe-inline\'; img-src \'self\' data: blob: https:; font-src \'self\' data:; connect-src \'self\'; frame-ancestors \'self\'; base-uri \'self\'; form-action \'self\'' },
+          {
+            key: 'Content-Security-Policy',
+            value:
+              "default-src 'self'; " +
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://mc.yandex.ru https://mc.yandex.com https://yastatic.net; " +
+              "style-src 'self' 'unsafe-inline'; " +
+              "img-src 'self' data: blob: https:; " +
+              "font-src 'self' data:; " +
+              "connect-src 'self' https://mc.yandex.ru https://mc.yandex.com wss://mc.yandex.ru wss://mc.yandex.com; " +
+              "frame-src 'self' https://vkvideo.ru https://*.vkvideo.ru https://vk.com https://*.vk.com; " +
+              "frame-ancestors 'self'; " +
+              "base-uri 'self'; " +
+              "form-action 'self'",
+          },
         ],
       },
     ]
